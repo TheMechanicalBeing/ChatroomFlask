cddecfce664dd9c116ac70f88690467494711670
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2023-07-24 00:29:59 +0400

    updated register.html, login.html and routes.py

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 I accidentally deleted main content of this project so I am going to write it again.
 
-On this commit I made initialization, made project structure, some templates, forms for verification and registration and for now I am going to add login
+Commit 7: I made initialization, made project structure, some templates, forms for verification and registration and for now I am going to add login
+
+Commit 8: I updated code of registration and login page.
--- mainContent/routes.py
+++ mainContent/routes.py
@@ -1,20 +1,28 @@
 from mainContent import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from mainContent.forms import RegistrationForm, LoginForm
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('mainpage.html', title='Home')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created successfully', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Account logged in successfully!', 'success')
+        return redirect(url_for('home'))
+
     return render_template('login.html', title='Login', form=form)
