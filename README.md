6b5a683f4f66765d740f19ac04f9af96de4f6ff6
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2023-07-29 22:58:15 +0400

    Create profile_pics and default.jpg Improve account update Connect style.css

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 I accidentally deleted main content of this project, so I am going to write it again.
 
-Commit 7: I made initialization, made project structure, some templates, forms for verification and registration and for now I am going to add login
+Commit 7: I made initialization, made project structure, some templates, forms for verification and registration and for now I am going to add login.
 
 Commit 8: I updated code of registration and login page.
 
@@ -12,6 +12,8 @@ Commit 10: I added number input in register, because I forgot to add an age fiel
 
 Commit 11: I Improved register route and integrated it to database.
 
-Commit 12: I improved login system, added logging in and logging out
+Commit 12: I improved login system, added logging in and logging out.
 
 Commit 13: I added login requirements for some pages, because on some pages you need access.
+
+Commit 14: I added account updates, integrated code on image files by adding profile_pics directy and default.jpg, added another form for updates. also connected base.html to styles.css.
--- mainContent/forms.py
+++ mainContent/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
 from mainContent.models import User
@@ -30,3 +32,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
+    age = IntegerField('Age', validators=[NumberRange(min=1, max=150)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+               raise ValidationError("This username already exists, try another one.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("This email is already taken, try another one.")
